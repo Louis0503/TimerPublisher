ee07f848793cb61f51b407e0b5b6d5828e14b72d
LouisLiu <louis19820503@gmail.com>
2016-07-21 17:37:13 +0800

    PublisherAndSubscriber

---
+++ TimerPublisher/TimerPublisher/PublisherTracker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerPublisher
+{
+    public class PublisherTracker<T>: IObservable<T>
+    {
+        private List<IObserver<T>> _observers;
+        public PublisherTracker()
+        {
+            _observers = new List<IObserver<T>>();
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            if(!_observers.Contains(observer))
+                _observers.Add(observer);
+            return new Unsubscriber<T>(_observers, observer);
+        }
+    }
+}
---
+++ TimerPublisher/TimerPublisher/SubScriber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerPublisher
+{
+    public class SubScriber<T>: IObserver<T>
+    {
+        private IDisposable unsubscriber;
+
+        public SubScriber()
+        {
+
+        }
+
+        public virtual void Subscribe(IObservable<T> provider)
+        {
+            if(provider != null)
+                unsubscriber = provider.Subscribe(this);
+        }
+
+        public virtual void OnCompleted()
+        {
+            this.Unsubscribe();
+        }
+        public virtual void OnNext(T value)
+        {
+        }
+        public virtual void OnError(Exception e)
+        {
+            throw new SubScriberException(e.Message + this.GetType().ToString());
+        }
+
+        private void Unsubscribe()
+        {
+            unsubscriber.Dispose();
+        }
+
+        public class SubScriberException : Exception
+        {
+            internal SubScriberException(string errorMessage):base(errorMessage)
+            {
+            }
+        }
+    }
+}
